121eee8c718a6ed95611e57645ff937ce66dba8b
Chetan-reddy81 <chetanreddy8121@gmail.com>
2026-04-05 14:41:51 +0530

    Update AdminService.java

--- Spring/src/main/java/com/cricket/project/service/AdminService.java
+++ Spring/src/main/java/com/cricket/project/service/AdminService.java
@@ -1,12 +1,12 @@
-//package com.cricket.project.service;
-//
-//import org.springframework.stereotype.Service;
-//
-//import com.cricket.project.Exception.UserNotFound;
-//import com.cricket.project.entity.Admin;
-//
-//@Service
-//public interface AdminService {
-//
-//	boolean adminLog(Admin admin) throws UserNotFound;
-//}
+package com.cricket.project.service;
+
+import org.springframework.stereotype.Service;
+
+import com.cricket.project.Exception.UserNotFound;
+import com.cricket.project.entity.Admin;
+
+@Service
+public interface AdminService {
+
+	boolean adminLog(Admin admin) throws UserNotFound;
+}
