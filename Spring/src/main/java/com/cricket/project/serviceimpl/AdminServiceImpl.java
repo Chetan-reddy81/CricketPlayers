9c12173f8dc703c55828b0e51e124750e9b59afd
Chetan-reddy81 <chetanreddy8121@gmail.com>
2026-04-05 14:43:24 +0530

    Update AdminServiceImpl.java

--- Spring/src/main/java/com/cricket/project/serviceimpl/AdminServiceImpl.java
+++ Spring/src/main/java/com/cricket/project/serviceimpl/AdminServiceImpl.java
@@ -1,32 +1,32 @@
-//package com.cricket.project.serviceimpl;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//
-//import com.cricket.project.Exception.UserNotFound;
-//import com.cricket.project.entity.Admin;
-//import com.cricket.project.repositories.AdminRepository;
-//import com.cricket.project.service.AdminService;
-//
-//@Component
-//public class AdminServiceImpl implements AdminService{
-//	
-//	@Autowired
-//	AdminRepository repo;
-//
-//	@Override
-//	public boolean adminLog(Admin admin) throws UserNotFound {
-//		
-//		if(repo.findUser(admin.getName(), admin.getPassword())==null)
-//		{
-//			throw new UserNotFound("Incorrect credentials ");
-//		}
-//		
-//		return true;
-//		
-//	}
-//	
-//	
-//	
-//
-//}
+package com.cricket.project.serviceimpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.cricket.project.Exception.UserNotFound;
+import com.cricket.project.entity.Admin;
+import com.cricket.project.repositories.AdminRepository;
+import com.cricket.project.service.AdminService;
+
+@Component
+public class AdminServiceImpl implements AdminService{
+	
+	@Autowired
+	AdminRepository repo;
+
+	@Override
+	public boolean adminLog(Admin admin) throws UserNotFound {
+		
+		if(repo.findUser(admin.getName(), admin.getPassword())==null)
+		{
+			throw new UserNotFound("Incorrect credentials ");
+		}
+		
+		return true;
+		
+	}
+	
+	
+	
+
+}
