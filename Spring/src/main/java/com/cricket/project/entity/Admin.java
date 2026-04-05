29b823119146c21917583f43a9c9e33355b92920
Chetan-reddy81 <chetanreddy8121@gmail.com>
2026-04-05 14:39:29 +0530

    Update Admin.java

--- Spring/src/main/java/com/cricket/project/entity/Admin.java
+++ Spring/src/main/java/com/cricket/project/entity/Admin.java
@@ -1,45 +1,45 @@
-//package com.cricket.project.entity;
+package com.cricket.project.entity;
 
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.Id;
-//
-//@Entity(name = "admin")
-//public class Admin {
-//
-//	@Id
-//	@GeneratedValue
-//	int id;
-//	String name;
-//	String password;
-//	public Admin() {
-//		super();
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity(name = "admin")
+public class Admin {
+
+	@Id
+	@GeneratedValue
+	int id;
+	String name;
+	String password;
+	public Admin() {
+		super();
 //		// TODO Auto-generated constructor stub
-//	}
-//	public Admin(int id, String name, String password) {
-//		super();
-//		this.id = id;
-//		this.name = name;
-//		this.password = password;
-//	}
-//	public int getId() {
-//		return id;
-//	}
-//	public void setId(int id) {
-//		this.id = id;
-//	}
-//	public String getName() {
-//		return name;
-//	}
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	public String getPassword() {
-//		return password;
-//	}
-//	public void setPassword(String password) {
-//		this.password = password;
-//	}
-//	
-//	
+	}
+	public Admin(int id, String name, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.password = password;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 //}
