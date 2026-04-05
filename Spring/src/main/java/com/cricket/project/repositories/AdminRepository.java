ae03bcdca245115c826ea8a2b5864792f5870bfd
Chetan-reddy81 <chetanreddy8121@gmail.com>
2026-04-05 14:40:48 +0530

    Update AdminRepository.java

--- Spring/src/main/java/com/cricket/project/repositories/AdminRepository.java
+++ Spring/src/main/java/com/cricket/project/repositories/AdminRepository.java
@@ -1,15 +1,15 @@
-//package com.cricket.project.repositories;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.stereotype.Repository;
-//
-//import com.cricket.project.entity.Admin;
-//
-//@Repository
-//public interface AdminRepository extends JpaRepository<Admin, Integer> {
-//
-//	
-//	@Query("select a from admin a where a.name=?1 and a.password=?2")
-//	Admin findUser(String name,String password);
-//}
+package com.cricket.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.cricket.project.entity.Admin;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+
+	
+	@Query("select a from admin a where a.name=?1 and a.password=?2")
+	Admin findUser(String name,String password);
+}
