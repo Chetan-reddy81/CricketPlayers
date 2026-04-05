d84bbb8c50f451f0396bb1fc16eecab600e9aff7
Chetan-reddy81 <chetanreddy8121@gmail.com>
2026-04-05 14:35:15 +0530

    Update AdminController.java

--- Spring/src/main/java/com/cricket/project/controller/AdminController.java
+++ Spring/src/main/java/com/cricket/project/controller/AdminController.java
@@ -1,39 +1,39 @@
-//package com.cricket.project.controller;
+package com.cricket.project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cricket.project.Exception.UserNotFound;
+import com.cricket.project.entity.Admin;
+import com.cricket.project.service.AdminService;
 //
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class AdminController {
 //
-//import com.cricket.project.Exception.UserNotFound;
-//import com.cricket.project.entity.Admin;
-//import com.cricket.project.service.AdminService;
-//
-//@RestController
-//@CrossOrigin(origins = "http://localhost:4200")
-//public class AdminController {
-//
-//	@Autowired
-//	AdminService as;
-//	@PostMapping("/adminLogin")
-//	ResponseEntity<?> admin(@RequestBody Admin admin)
-//	{
-//		try
-//	     {
-//	    	 as.adminLog(admin);
-//	     }
-//	     catch (UserNotFound e) {
-//			// TODO: handle exception
-//			 return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND); 
-//		}
-//	     catch (Exception e) {
-//			// TODO: handle exception
-//	    	 return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_ACCEPTABLE); 
-//		}
-//	     
-//		 return new ResponseEntity<>(HttpStatus.ACCEPTED);
-//	}
+	@Autowired
+	AdminService as;
+	@PostMapping("/adminLogin")
+	ResponseEntity<?> admin(@RequestBody Admin admin)
+	{
+		try
+	     {
+	    	 as.adminLog(admin);
+	     }
+	     catch (UserNotFound e) {
+			 TODO: handle exception
+			 return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND); 
+		}
+	     catch (Exception e) {
+			 TODO: handle exception
+	    	 return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_ACCEPTABLE); 
+		}
+	     
+		 return new ResponseEntity<>(HttpStatus.ACCEPTED);
+	}
 //}
